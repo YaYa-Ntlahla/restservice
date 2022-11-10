459f8c72abea9f98701967cadc24324565308b9f
yaya_code <ntlahlayandisa@gmail.com>
2022-11-10 22:51:46 +0200

    Initial commit

---
+++ rest-service/src/main/java/com/example/rest/service/GreetingController.java
@@ -0,0 +1,16 @@
+package com.example.rest.service;
+
+
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class GreetingController {
+    private static final String template ="Hello,%s!";
+    private final AtomicLong counter =  new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting (RequestParam(value= "name", defaultValue = "World") String name)
+
+    {
+        return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
+}
---
+++ rest-service/src/main/java/com/example/rest/service/RestController.java
@@ -0,0 +1,2 @@
+package com.example.rest.service;public @interface RestController {
+}
